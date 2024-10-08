22fa661691f10b3b0a36eab70e81af29a240b018
hetvi394 <hetvishah394@gmail.com>
2024-10-08 12:53:49 +0530

    chanegs

--- src/index.js
+++ src/index.js
@@ -6,21 +6,3 @@ dotenv.config({
   path: "./env",
 });
 connectDB();
-
-// import express from "express";
-// const app = "express"
-// (async () => {
-//   try {
-//     monngoose.export(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     app.on("ERROR:", () => {
-//       console.log("ERROR:", err);
-//       throw err;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("ERROR:", error);
-//     throw err;
-//   }
-// })();
